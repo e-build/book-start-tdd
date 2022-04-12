4d767f1fa9af8a086931e0f60a20eb8f1f96521a
e-build <donggeol2@daum.net>
2022-04-12 21:40:42 +0900

    Chore : gradle 프록제트 세팅

---
+++ src/main/java/com/ebuild/bookstarttdd/BookStartTddApplication.java
@@ -0,0 +1,13 @@
+package com.ebuild.bookstarttdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStartTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStartTddApplication.class, args);
+    }
+
+}
